82fd9e03d6ec23a903649e8c9745096b6cb08f85
tsalzman <tobias.salzmann@otto.de>
2021-12-08 23:26:56 +0100

    Day 8

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import qualified Day4
 import qualified Day5
 import qualified Day6
 import qualified Day7
+import qualified Day8
 import Lib
 
 main :: IO ()
@@ -23,4 +24,5 @@ runDay 4 = runConfig Day4.config
 runDay 5 = runConfig Day5.config
 runDay 6 = runConfig Day6.config
 runDay 7 = runConfig Day7.config
+runDay 8 = runConfig Day8.config
 
---
+++ src/Day8.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE NamedFieldPuns #-}
+module Day8 (config) where
+  
+import Lib
+import Data.List
+import qualified Data.Set as Set
+import Data.Maybe (fromJust)
+import Data.Char (intToDigit, ord)
+
+config :: Config [Display] Int Int
+config = Config {title = "Day 8"
+                ,input = fmap (map parseDisplay) $ parseWordLists "resources/day8.txt"
+                ,part1 = solvePart1
+                ,part2 = solvePart2}
+
+data Display = Display{patterns :: [String], output :: [String]}
+
+parseDisplay :: [String] -> Display
+parseDisplay inp = case span (/= "|") inp of
+  (pat, _ : out) -> Display{patterns=pat, output=out}
+  _ -> error "invalid input"
+
+solvePart1 :: [Display] -> Int
+solvePart1 =  length . filter (`elem` [2,3,4,7]) . map length . concatMap output
+
+solvePart2 :: [Display] -> Int
+solvePart2 = sum . (map readDisplay)
+
+readDisplay :: Display -> Int
+readDisplay (Display{patterns, output}) = readDisplayUsingPerm (deducePerm patterns) output
+
+readDisplayUsingPerm :: String -> [String] -> Int
+readDisplayUsingPerm perm = read . map (intToDigit . fromJust . (`elemIndex` digits) . applyPerm perm) 
+
+digits :: [String] 
+digits = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
+
+applyPerm :: String -> String -> String
+applyPerm perm pattern = sort . map (toFunc' perm) $ pattern
+      
+toFunc' :: String -> Char -> Char
+toFunc' perm c = perm !! (ord c - ord 'a')
+
+deducePerm :: [String] -> String
+deducePerm patterns = 
+  let [pat2, pat3, pat4, _, _, _, pat61, pat62, pat63, pat7] = sortOn length patterns
+      cf = pat2 
+      a = pat3 \\ cf
+      bd = pat4 \\ cf
+      eg = pat7 \\ (cf ++ a ++ bd)
+      [g] = [y | x <- [pat61, pat62, pat63], let y = intersect eg x, length y == 1]
+--      [g] = filter ((==1) . length). map (intersect eg ) $ [pat61, pat62, pat63]
+      [f] = filter ((==1) . length) . map (intersect cf ) $ [pat61, pat62, pat63]
+      [b] = filter ((==1) . length) . map (intersect bd ) $ [pat61, pat62, pat63]
+  in invertPerm . concat $ [a, b, cf \\ f, bd \\ b, eg \\ g, f, g]
+  
+invertPerm :: String -> String
+invertPerm = map fst . sortOn snd . zip "abcdefg" 
+  
+  
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
