9aba78d998bb8f7210ace14c203adcd3669c941a
tsalzman <tobias.salzmann@otto.de>
2021-12-09 16:58:36 +0100

    Day 9 - remove redundant imports

--- src/Day9.hs
+++ src/Day9.hs
@@ -4,15 +4,14 @@ module Day9 (config) where
 import Lib
 import Data.List
 import qualified Data.Map as Map
-import Data.Maybe (fromJust)
-import Data.Char (digitToInt, ord)
+import Data.Char (digitToInt)
 
 config :: Config (Map.Map Point2D Int) Int Int
 config = Config {title = "Day 9"
                 ,input = parseMap digitToInt "resources/day9.txt"
                 ,part1 = solvePart1
                 ,part2 = solvePart2}
-                
+
 solvePart1 :: Map.Map Point2D Int -> Int
 solvePart1 m = sum . map (uncurry (riskLevel m)) . Map.toList $ m
 
@@ -20,26 +19,26 @@ riskLevel :: Map.Map Point2D Int -> Point2D -> Int -> Int
 riskLevel m p level
   | all (>level) (manhattanNeighbours m p) = level + 1
   | otherwise = 0
-    
+
 solvePart2 :: Map.Map Point2D Int -> Int
-solvePart2 m = 
+solvePart2 m =
   let initialBasins = Map.mapWithKey const . Map.filter (/=9) $ m
       finalBasins = mergeBasinsToFixedPoint initialBasins
       basinSizes = map length . group . sort . Map.elems $ finalBasins
   in product . take 3 . reverse . sort $ basinSizes
 
 mergeBasinsToFixedPoint :: Map.Map Point2D Point2D -> Map.Map Point2D Point2D
-mergeBasinsToFixedPoint m = 
+mergeBasinsToFixedPoint m =
   let nextM = mergeBasins m
   in if nextM == m then m else mergeBasinsToFixedPoint nextM
-    
+
 
 mergeBasins :: Map.Map Point2D Point2D -> Map.Map Point2D Point2D
 mergeBasins m = Map.mapWithKey (findMinimalBasin m) m
 
 findMinimalBasin :: Map.Map Point2D Point2D  -> Point2D -> Point2D -> Point2D
 findMinimalBasin m p pBasin = minimum $ pBasin : manhattanNeighbours m p
-  
+
   
 
 
